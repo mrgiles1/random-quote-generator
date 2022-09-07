bb58257c745fa3b867279b100a40779a67e72481
mrgiles1 <30458063+mrgiles1@users.noreply.github.com>
2022-09-07 18:31:47 +0200

    add files

---
+++ js/quotegen.js
@@ -0,0 +1,136 @@
+var quotes = [
+'Failure is an option here. If things are not failing, you are not innovating enough.',
+'Optimism, pessimism- fuck that. We\'re going to make it happen. As God as my bloody witness, I\'m hell-bent on making it work.',
+'If something is important enough, even if the odds are against you, you should still do it.',
+'There\'s a fundamental difference, if you look into the future, between a humanity that is a space-faring civilization, that\'s out there exploring the stars, compared with one where we are forever confined to Earth until some eventual extinction event.',
+'The thing that\'s worth doing is trying to improve our understanding of the world and gain a better appreciation of the universe and not to worry too much about there being no meaning. And, you know, try and enjoy yourself. Because, actually, life\'s pretty good. It really is.',
+'I came to the conclusion that we should aspire to increase the scope and scale of human consciousness in order to better understand what questions to ask. Really, the only thing that makes sense is to strive for greater collective enlightenment.',
+'You want to be extra rigorous about making the best possible thing you can. Find everything that is wrong with it and fix it. Seek negative feedback, particularly from friends.',
+'Given that this is the first time in 4.5 billion years where it\'s been possible for humanity to extend life beyond Earth, it seems like we would be wise to act while the window was open and not count on the fact it will be open a long time.',
+'I think it is possible for ordinary people to choose to be extraordinary.',
+'Some people don\'t like change, but you need to embrace change if the alternative is disaster.',
+'The path to the CEO\'s office should not be through the CFO\'s office, and it should not be through the marketing department. It needs to be through engineering and design.',
+'Persistence is very important. You should not give up unless you are forced to give up.',
+'A company is a group organized to create a product or service, and it is only as good as its people and how excited they are about creating. I do want to recognize a ton of super-talented people. I just happen to be the face of the companies.',
+'People work better when they know what the goal is and why. It is important that people look forward to coming to work in the morning and enjoy working.',
+'I think we have a duty to maintain the light of consciousness to make sure it continues into the future.',
+'I think the best way to attract venture capital is to try and come up with a demonstration of whatever product or service it is and ideally take that as far as you can. Just see if you can sell that to real customers and start generating some momentum. The further along you can get with that, the more likely you are to get funding.',
+'We have a strict \'no-assholes policy\' at SpaceX.',
+'When Henry Ford made cheap, reliable cars, people said, \'Nah, what\'s wrong with a horse?\' That was a huge bet he made, and it worked.',
+'I always have optimism, but I\'m realistic. It was not with the expectation of great success that I started Tesla or SpaceX... It\'s just that I thought they were important enough to do anyway.',
+'If you get up in the morning and think the future is going to be better, it is a bright day. Otherwise, it\'s not.'
+
+]
+
+var sQuotes = [
+'Sometimes I\'ll start a sentence and I don\'t even know where it\'s going. I just hope I find it along the way.',
+'Sometimes you have to take a break from being the kind of boss that\'s always trying to teach people things. Sometimes you just have to be the boss of dancing.',
+'Fool me once, strike one. Fool me twice, strike three.',
+'If I had a gun with two bullets and I was in a room with Hitler, Bin Laden and Toby, I would shoot Toby twice.',
+'Would I rather be feared or loved? Easy. Both. I want people to be afraid of how much they love me. ',
+'The worst thing about prison was the dementors.',
+'I hate so much about the things that you choose to be.',
+'You may look around and see two groups here: white collar, blue collar. But I don\'t see it that way, and you know why not? Because I am collar-blind. ',
+'It\'s not like booze ever killed anyone. ',
+'I love inside jokes. I\'d love to be a part of one someday.',
+'Don\'t ever, for any reason, do anything for anyone, for any reason, ever, no matter what. No matter where. Or who, or who you are with, or where you are going or where you\'ve been- ever. For any reason, whatsoever.',
+'It\'s simply beyond words. It\'s incalculable.',
+'Hate to see you leave but love to watch you go. \'Cause of your butt.',
+'Oh God, my mind is going a mile an hour.',
+'I\'m not superstitious but I am a little stitious.',
+'I don\'t hate it. I just don\'t like it at all and it\'s terrible.',
+'I would not miss it for the world. But if something else came up I would definitely not go. ',
+'There is no such thing as an appropriate joke, that\'s why it\'s a joke.',
+'Tell him to call me ASAP as possible.',
+'Jim and I are great friends. We hang out a ton, mostly at work.'
+
+]
+
+var dQuotes = [
+	'You know I am automatically attracted to beautiful ... I just start kissing them. It is like a magnet. Just kiss. I do not even wait. And when you are a star, they let you do it. You can do anything. Grab them by the pussy. You can do anything.',
+	'Nobody respects women more than me.',
+	'Why can\'t we use nuclear weapons?',
+	'I have been treated very unfairly by this judge. Now, this judge is of Mexican heritage. I\'m building a wall, OK? I\'m building a wall.',
+	'I\'m also honored to have the greatest temperament that anybody has.',
+	'I don\'t think Ivanka would do that, although she does have a very nice figure. I\'ve said if Ivanka weren\'t my daughter, perhaps I\'d be dating her.',
+	'Women: You have to treat them like shit.',
+	'I sorta get away with things like that.',
+	'I could stand in the middle of Fifth Avenue and shoot somebody, and I wouldn\'t lose any voters, okay? It\'s, like, incredible.',
+	'We won with poorly educated. I love the poorly educated.',
+	'We should just cancel the election and just give it to Trump.',
+	'I know more about ISIS than the generals do, believe me. I would bomb the shit out of them.',
+	'He referred to my hands, if they\'re small, something else must be small. I guarantee you there\'s no problem. I guarantee it.',
+	'You could see there was blood coming out of her eyes. Blood coming out of her wherever.',
+	'Happy Cinco de Mayo! The best taco bowls are made in Trump Tower Grill. I love Hispanics!',
+	'It is better to live one day as a lion than 100 years as a sheep.',
+	'I will build a great wall, and nobody builds walls better than me, believe me. And I\'ll build them very inexpensively. I will build a great, great wall on our southern border, and I will make Mexico pay for that wall. Mark my words.',
+	'The beauty of me is that I\'m very rich.',
+	'I like kids. I mean, I will not do anything to take care of them. I will supply funds, and she will take care of the kids.',
+	'I have a great relationship with the blacks.'
+]
+
+var bQuotes = [
+	'When the student is ready, the teacher will appear.',
+	'The less you respond to negative people, the more peaceful your life will become.',
+	'Health is the greatest gift, contentment is the greatest wealth, A trusted friend is the best relative, liberated mind is the greatest bliss.',
+	'The thought manifests as the word: the word manifests as the deed: the deed develops into character. So watch the thought and its ways with care, and let it spring from love born out of concern for all beings.',
+	'Do not learn how to react learn how to respond.',
+	'If your compassion does not include yourself, It is incomplete.',
+	'Everything that has a begining has an ending. Make your peace with that and all will be well.',
+	'If anything is worth doing, do it with all your heart.',
+	'Your worst enemy cannot harm you as much as your own unguarded thoughts.',
+	'Holding onto anger is like drinking poison and expecting the other person to die.',
+	'All that we are is the result of what we have thought.',
+	'What you think you become; what you feel, you attract. What you imagine, you create.',
+	'Relax. Nothing is in control.',
+	'Your work is to discover your world and then with all your heart give yourself to it.',
+	'Believe nothing. No matter where you read it or who has said it - not even if I have said it - unless it agrees with your own reason and your own common sense.',
+	'To understand everything is to forgive everything.',
+	'No one saves us but ourselves. No one can and no one may. We ourselves must walk the path.',
+	'There is no path to happiness. Happiness is the path.',
+	'No matter how hard the past, you can always begin again.',
+	'If you want to fly, give up everything that weighs you down.'
+]
+
+
+
+function newQuote() {
+
+	var randomNumber = Math.floor(Math.random()* (quotes.length));
+	document.getElementById('quoteDisplay').innerHTML = "<br><br><p style='text-align: right;'>\"" + quotes[randomNumber] + "\"</p><p class='cite'><em>-Elon Musk </em></p>";
+	// $(.background).css('background-image','url(../images/elonmusk01.jpg)');
+
+	document.getElementById('bg').style.background = "url('images/elonmusk04.jpg')no-repeat top"; 
+	
+}
+
+function scottQuote() {
+
+	var randomNumber = Math.floor(Math.random()* (quotes.length));
+	document.getElementById('quoteDisplay').innerHTML = "<br><br><p>\"" + sQuotes[randomNumber] + "\"</p><p class='cite'><em>-Michael Scott </em></p>";
+	// $(.background).css('background-image','url(../images/mscott01.jpg)');
+	document.getElementById('bg').style.background = "url('images/mscott01.jpg')no-repeat top";
+
+}
+
+function buddhaQuote() {
+
+	var randomNumber = Math.floor(Math.random()* (quotes.length));
+	document.getElementById('quoteDisplay').innerHTML = "<br><br><p>\"" + bQuotes[randomNumber] + "\"</p><p class='cite'><em>-Buddha </em></p>";
+	// $(.background).css('background-image','url(../images/mscott01.jpg)');
+	document.getElementById('bg').style.background = "url('images/buddha01.jpg')no-repeat top";
+
+}
+
+newQuote();
+
+// $(document).ready(function() {
+//   newQuote();
+//   $('#quoteButton').on('click', newQuote);
+//   $('#tweetQuote').on('click', function() {
+//     if(!inIframe()) {
+//       openURL('https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + currentQuote + '"'));
+//     }
+//   });
+
+// }
